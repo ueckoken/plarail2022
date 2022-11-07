ea07b397aa79b2e2bf22dd530e573f70180d2052
VOID <kouichi.hirachi@gmail.com>
2022-11-07 18:33:25 +0900

    fix protoc sh

--- backend/auto_operation/central_controller/protoc-gen.sh
+++ backend/auto_operation/central_controller/protoc-gen.sh
@@ -9,11 +9,7 @@ python3 -m grpc_tools.protoc \
   ../../proto/*.proto
 
 # change import to from . import on block_pb2_grpc.py
-sed -i -e 's/import block_pb2/from . import block_pb2/g' ./spec/block_pb2_grpc.py
-rm ./spec/block_pb2_grpc.py-e
-sed -i -e 's/import ats_pb2/from . import ats_pb2/g' ./spec/ats_pb2_grpc.py
-rm ./spec/ats_pb2_grpc.py-e
-sed -i -e 's/import statesync_pb2/from . import statesync_pb2/g' ./spec/statesync_pb2_grpc.py
-rm ./spec/statesync_pb2_grpc.py-e
-sed -i -e 's/import speedControl_pb2/from . import speedControl_pb2/g' ./spec/speedControl_pb2_grpc.py
-rm ./spec/speedControl_pb2_grpc.py-e
+gsed -i 's/import block_pb2/from . import block_pb2/g' ./spec/block_pb2_grpc.py
+gsed -i 's/import ats_pb2/from . import ats_pb2/g' ./spec/ats_pb2_grpc.py
+gsed -i 's/import statesync_pb2/from . import statesync_pb2/g' ./spec/statesync_pb2_grpc.py
+gsed -i 's/import speedControl_pb2/from . import speedControl_pb2/g' ./spec/speedControl_pb2_grpc.py
