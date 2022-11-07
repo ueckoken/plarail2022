b180866152d5caf8fc17d93a7ae6fa39f62dace6
VOID <kouichi.hirachi@gmail.com>
2022-11-07 18:27:42 +0900

    fix prot gen sh

--- backend/auto_operation/central_controller/protoc-gen.sh
+++ backend/auto_operation/central_controller/protoc-gen.sh
@@ -7,3 +7,13 @@ python3 -m grpc_tools.protoc \
   --grpc_python_out=./spec \
   -I ../../proto/ \
   ../../proto/*.proto
+
+# change import to from . import on block_pb2_grpc.py
+sed -i -e 's/import block_pb2/from . import block_pb2/g' ./spec/block_pb2_grpc.py
+rm ./spec/block_pb2_grpc.py-e
+sed -i -e 's/import ats_pb2/from . import ats_pb2/g' ./spec/ats_pb2_grpc.py
+rm ./spec/ats_pb2_grpc.py-e
+sed -i -e 's/import statesync_pb2/from . import statesync_pb2/g' ./spec/statesync_pb2_grpc.py
+rm ./spec/statesync_pb2_grpc.py-e
+sed -i -e 's/import speedControl_pb2/from . import speedControl_pb2/g' ./spec/speedControl_pb2_grpc.py
+rm ./spec/speedControl_pb2_grpc.py-e
