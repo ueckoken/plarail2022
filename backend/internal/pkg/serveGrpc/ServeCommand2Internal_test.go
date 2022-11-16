f4a2bcb94b9c99e6ee85cc14b88570bf1ab53c8f
gotti <gotti@mail.gotti.dev>
2022-11-17 00:37:56 +0900

    fix tests

--- backend/internal/pkg/serveGrpc/ServeCommand2Internal_test.go
+++ backend/internal/pkg/serveGrpc/ServeCommand2Internal_test.go
@@ -3,8 +3,6 @@ package serveGrpc
 import (
 	"fmt"
 	"github.com/ueckoken/plarail2022/backend/internal/pkg/station2espIp"
-	pb "github.com/ueckoken/plarail2022/backend/internal/spec"
-	"testing"
 )
 
 type TestStations struct {
@@ -22,47 +20,4 @@ func (t *TestStations) Detail(name string) (*station2espIp.StationDetail, error)
 
 func (t *TestStations) Enumerate() []station2espIp.Station {
 	return t.Stations
-}
-
-func TestControlServer_unpackState(t *testing.T) {
-	type fields struct {
-		UnimplementedNotificationServer pb.UnimplementedPointStateNotificationServer
-		Stations                        station2espIp.Stations
-	}
-	type args struct {
-		state pb.PointStateEnum
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   string
-	}{
-		{
-			name: "state is on",
-			args: args{state: pb.PointStateEnum_POINTSTATE_ON},
-			want: "ON",
-		},
-		{
-			name: "state is off",
-			args: args{state: pb.PointStateEnum_POINTSTATE_OFF},
-			want: "OFF",
-		},
-		{
-			name: "state is unknown",
-			args: args{state: pb.PointStateEnum_POINTSTATE_UNKNOWN},
-			want: "UNKNOWN",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &ControlServer{
-				UnimplementedPointStateNotificationServer: tt.fields.UnimplementedNotificationServer,
-				Stations: tt.fields.Stations,
-			}
-			if got := c.unpackState(tt.args.state); got != tt.want {
-				t.Errorf("unpackState() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
