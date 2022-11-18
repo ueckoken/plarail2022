ab93140e797161a2d4d5a211ee26c33f0b749f79
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-11-18 18:15:06 +0900

    fix

--- backend/auto_operation/gRPCTestClient.py
+++ backend/auto_operation/gRPCTestClient.py
@@ -6,10 +6,6 @@ import grpc
 
 import spec.ats_pb2 as ats_pb2
 import spec.ats_pb2_grpc as ats_pb2_grpc
-import spec.block_pb2 as block_pb2
-import spec.block_pb2_grpc as block_pb2_grpc
-import spec.statesync_pb2 as statesync_pb2
-import spec.statesync_pb2_grpc as statesync_pb2_grpc
 
 cred = grpc.ssl_channel_credentials(root_certificates=bytes(certifi.contents(), "utf-8"))
 
