ae58d883955504e629f7ddc54692d2a7b36e561e
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-11-18 11:17:27 +0900

    fix merge miss

--- backend/external/pkg/envStore/envStore.go
+++ backend/external/pkg/envStore/envStore.go
@@ -37,10 +37,9 @@ func (p *Port) Unmarshal(s string) error {
 
 type Env struct {
 	ClientSideServer struct {
-		PointStatePort Port `envconfig:"default=54320"`
-		BlockStatePort Port `envconfig:"default=54321"`
-		GrpcPort       Port `envconfig:"default=9000"`
-		ATSAddress     hostnamePort
+		Port       Port `envconfig:"default=54321"`
+		GrpcPort   Port `envconfig:"default=9000"`
+		ATSAddress hostnamePort
 	}
 	InternalServer struct {
 		Addr        hostnamePort
